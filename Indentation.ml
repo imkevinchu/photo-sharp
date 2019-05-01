b0b9951c44528535c014f1e3eba0bf257615b9a5
jancy <jancycapellan97@gmail.com>
2019-05-01 12:15:30 -0400

    Updated logic to using fold_left

--- Indentation.ml
+++ Indentation.ml
@@ -11,19 +11,28 @@ let indentations filename =
     done; !lines
   with End_of_file ->
     close_in channel;
-    List.rev !lines
-    in 
+    List.rev !lines 
     
-    let level = 0 in
-    let tabCount = 0 in
-    let n = 0 in
-    let previous = ref 0 in 
+    
+    let rec indentCount inputString n count = 
+      if inputString.[n] = '\t' then indentCount inputString (n+1) (count+1)
+      else count       
+    in
 
-    let f inputString =
-      if inputString.[n] = ['\t'] & tabCount != 0 then incr tabCount
-      if tabCount > previous then append to string dedent \n } ] \n to the far
-      if tabCount < previous  then append to string indent \n { \n to the beginning of the line  
-        
-      incr n in 
+    let f (previous_n, lines_so_far) line =
+      let new_tabCount =  indentCount line 0 0 in
+      let new_line = 
+        if tabCount > previous_n then append to string dedent \n } ] \n to the far
+        inputstring :: '\n}\n'
+        if tabCount < previous  then append to string indent \n { \n to the beginning of the line 
+        in
+      let new_lines = lines_so_far :: new_line in
+      (new_n, new_lines)
+    
+    in
+    
+    let (tabcount,revNewList) = List.fold_left f (0,[]) !lines
 
-      List.iter f lines ;;
+    in 
+
+    String.concat "\n" (List.rev revNewList)
