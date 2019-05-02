9a78b6133f837fb140a89f10462db8e94054bb02
mblockk <43766061+mblockk@users.noreply.github.com>
2019-05-02 13:18:18 -0400

    fixed add gaussian noise function

--- psharplib.c
+++ psharplib.c
@@ -144,25 +144,104 @@ struct ImageArr* reflect(struct ImageArr *m){
     }
     return m;
 }
+/*
+
+    **** O(n^4) -> not tractable on large image files ****
+
+void DConv(struct ImageArr *in, struct ImageArr *mask, struct ImageArr *out){
+
+    long i,j,m,n,idx,jdx;
+    int ms1,ms2,ms3,ms4,im1,im2,im3,im4,val1,val2,val3,val4;
+    struct pixel *tmp;
+
+    for(i=0; i<in->h; ++i)
+        for(j=0; j< in->w; ++j){
+            val1 = val2 = val3 = val4 = 0;
+            for(m=0; m<mask->h; ++m)
+                for(n =0; n<mask->w; ++n){
+                    ms1 = mask->imgPixelData[m*mask->h+n]->red;
+                    ms2 = mask->imgPixelData[m*mask->h+n]->green;
+                    ms3 = mask->imgPixelData[m*mask->h+n]->blue;
+                    ms4 = mask->imgPixelData[m*mask->h+n]->alpha;
+                idx = i-m;
+                jdx = j-n;
+                if(idx >=0 && jdx >=0){
+                    im1 = in->imgPixelData[idx*in->h+jdx]->red;
+                    im2 = in->imgPixelData[idx*in->h+jdx]->green;
+                    im3 = in->imgPixelData[idx*in->h+jdx]->blue;
+                    im4 = in->imgPixelData[idx*in->h+jdx]->alpha;
+                }
+                val1 += ms1*im1;
+                val2 += ms2*im2;
+                val3 += ms3*im3;
+                val4 += ms4*im4;
+
+                }
+            if(val1 > 255) val1 = 255;
+            if(val2 > 255) val2 = 255;
+            if(val3 > 255) val3 = 255;
+            if(val4 > 255) val4 = 255;
+            if(val1 < 0) val1 = 0;
+            if(val2 < 0) val2 = 0;
+            if(val3 < 0) val3 = 0;
+            if(val4 < 0) val4 = 0;
+            printf("VAL1: %d\n", val1);
+            printf("VAL2: %d\n", val1);
+            printf("VAL3: %d\n", val1);
+            printf("VAL4: %d\n", val1);
+            tmp->red = out->imgPixelData[i*out->h+j]->red;
+            tmp->green = out->imgPixelData[i*out->h+j]->green;
+            tmp->blue = out->imgPixelData[i*out->h+j]->blue;
+            tmp->blue = out->imgPixelData[i*out->h+j]->alpha;
+            tmp->red = (int)val1;
+            tmp->green = (int)val2;
+            tmp->blue = (int)val3;
+            tmp->alpha = (int)val4;
+        }
+}
+*/
 
 struct ImageArr* addNoise(struct ImageArr *Im, float var, float mean){
+//http://adaptiveart.eecs.umich.edu/2011/wp-content/uploads/2011/09/The-pocket-handbook-of-image-processing-algorithms-in-C.pdf
     /// NEEDS WORK -- SEG FAULTING // 
-    int x,y;
+    int x,y,z,r;
+    int cur = 0;
     float noise, theta;
-    for(y =0; y< Im->h; y++){
-        for(x = 0; x< Im->w; x++){
-        noise = sqrt(-2 * var * log(1.0-(float)rand()/32767.1));
-        theta = (float)rand()*1.917534E-4 - 3.14159265;
-        noise = noise * cos(theta);
-        noise = noise + mean;
-        if(noise > 255)
-            noise = 255;
-        if (noise <0)
-            noise = 0;
-
-
-       // *(Im->imgPixelData+x+(long)y*Im->w) = (int)(noise +.5);       
+    for(y =0; y < Im->h; y++){
+        for(x = 0; x < Im->w; x++){
+            
+                r = rand()%32767;
+        
+                noise = sqrt(-2 * var * log(1.0-(float)r/32767.1));
+                r = rand()%32767;
+                theta = (float)r*1.9175345E-4 - 3.14159265;
+                noise = noise * cos(theta);
+                noise = noise + mean;
+                if(noise > 255)
+                noise = 255;
+                if (noise < 0)
+                noise = 0;
+                printf("%f\n", noise);
+                cur = rand()%3;
+                if (cur == 0){ 
+                    Im->imgPixelData[x+(long)y*Im->w]->red = (int)(noise +.5); 
+                    
+                }
+                else if (cur == 1) {
+                    Im->imgPixelData[x+(long)y*Im->w]->green = (int)(noise +.5);
+                   
+                }
+                else if (cur == 2) {
+                    Im->imgPixelData[x+(long)y*Im->w]->blue = (int)(noise +.5);
+                 
+                }
+                else if (cur == 3){
+                    Im->imgPixelData[x+(long)y*Im->w]->alpha = (int)(noise +.5);
+                }
+       
+        
         //printf("%d\n",y);
+    
     }
     }
     return Im;
@@ -174,17 +253,20 @@ struct ImageArr* addNoise(struct ImageArr *Im, float var, float mean){
 int main() {
  
     struct ImageArr *img;
-    img = open("babboon.jpg");
+    struct ImageArr *img2;
+    struct ImageArr *img3;
+    img = open("testsaturate2.jpg");
+    img2 = open("tree.jpg");
+    img3 = open("reflected.jpg");
     //img = Image(400,400);
-    //printImageArr(img);
     //flip(img);
     //reflect(img);
-    testSatPixel(img, 150);
-    //addNoise(img, 800, 0);
+    //testSatPixel(img, 150);
+    //img = addNoise( img, 10000, 100);
     //save("noise.jpg", img);
-
+    DConv(img,img2,img3);
     //struct pixel *p;
     //p = setPix(255,255,255,100);
-    save("testsaturatepixel.jpg", img);  
+    save("dc.jpg", img3);  
     //free(img);
 }
